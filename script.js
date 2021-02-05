372b9b41fb967bd12a53b01a38dbbca249de060e
Sakib12345 <sakibalislam12345@gmail.com>
2021-02-06 04:13:33 +0600

    almost done

---
+++ script.js
@@ -0,0 +1,48 @@
+// const container = document.getElementById('container')
+// const div = document.createElement('div')
+// console.log(container.appendChild(div))
+
+fetch('https://restcountries.eu/rest/v2/all')
+.then(res => res.json())
+.then(data => displayCountries(data));
+
+
+const displayCountries = (countries) => {
+    const countriesContainer = document.getElementById('countries')
+    countries.forEach(country => {
+        const countryDiv = document.createElement('div')
+        countryDiv.className = 'country';
+
+        const countryInfo = `
+        <h3 class="country-name">${country.name}</h3>
+        <p>${country.capital}</p>
+        `
+        countryDiv.innerHTML = countryInfo
+        countriesContainer.appendChild(countryDiv);
+    });
+
+
+
+    // for (let i = 0; i < countries.length; i++) {
+    //     const country = countries[i];
+    //     const countryDiv = document.createElement('div')
+    //     countryDiv.className = 'country';
+
+    //     const countryInfo = `
+    //     <h3 class="country-name">${country.name}</h3>
+    //     <p>${country.capital}</p>
+    //     `
+    //     countryDiv.innerHTML = countryInfo
+    //     countriesContainer.appendChild(countryDiv);
+
+    //     // const name = document.createElement('h3')
+    //     // const capital = document.createElement('p')
+
+    //     // name.innerText = country.name;
+    //     // capital.innerText = country.capital
+
+    //     // countryDiv.appendChild(name)
+    //     // countryDiv.appendChild(capital)
+
+    // }
+}
